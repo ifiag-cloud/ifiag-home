d4b9c2eb0e1781191964cba9891610efad69257b
mridha-emran <bari.lala490@gmail.com>
2022-02-28 16:03:53 +0100

    deploy test done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import Footer from './Components/Footers/Footer';
 import './App.css';
-// import SecoundPage from './Views/SecoundPage';
+import SecoundPage from './Views/SecoundPage';
 import NewHeader from './Components/Headers/NewhHeader';
-import Accueil from './secoundViws/accueil/Accueil';
-import Institut from './secoundViws/institut/Institut';
-import DigiMobile from './secoundViws/digiMobile/DigiMobile';
 
 function App() {
   return (
@@ -14,11 +11,7 @@ function App() {
           <BrowserRouter>
           <div className="App">
             <NewHeader />
-              <Switch>
-                <Route exact path="/" component={Accueil} />
-                <Route exact path="/institut" component={Institut} />
-                <Route exact path="/digimobile" component={DigiMobile} />
-              </Switch>
+              <SecoundPage/>
             <Footer />
           </div>
           </BrowserRouter>
